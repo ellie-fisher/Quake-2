ab9e1e9e265f2788efc57906f3308bb48b5d622d
ellie-fisher <ellie.am.fisher@gmail.com>
2026-08-28 01:05:44 -0700

    Get the game running in a basic state

---
+++ src/server/sv_null.c
@@ -0,0 +1,17 @@
+﻿// sv_null.c -- this file can stub out the entire server system
+// for pure net-only clients
+
+#include "qcommon/qcommon.h"
+
+void SV_Init (void)
+{
+}
+
+void SV_Shutdown (char *finalmsg, qboolean reconnect)
+{
+}
+
+void SV_Frame (float time)
+{
+}
+
